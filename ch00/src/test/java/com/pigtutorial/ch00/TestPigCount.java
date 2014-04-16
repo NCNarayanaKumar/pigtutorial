b7d11338f9efb576aabe89b992c8405e0e6c71bf
Suraj Nayak <suraj@datametica.com>
2014-04-17 03:48:43 +0530

    Added First Apache Pig PigUnit test case and gitignore configurations

---
+++ ch00/src/test/java/com/pigtutorial/ch00/TestPigCount.java
@@ -0,0 +1,48 @@
+package com.pigtutorial.ch00;
+
+import static org.apache.pig.builtin.mock.Storage.resetData;
+import static org.apache.pig.builtin.mock.Storage.tuple;
+
+import java.util.Iterator;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.apache.pig.ExecType;
+import org.apache.pig.PigServer;
+import org.apache.pig.builtin.mock.Storage.Data;
+import org.apache.pig.data.Tuple;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+public class TestPigCount extends TestCase {
+	private static PigServer pigServer;
+
+    @Before
+    public void setUp() throws Exception {
+        pigServer = new PigServer(ExecType.LOCAL);
+    }
+
+    @Test
+    public void testAllCurrentTimeSame() throws Exception {
+        Data data = resetData(pigServer);
+
+        List<Tuple> dummySequenceNumbers = Lists.newArrayList();
+        for (int i = 0; i < 1000; i++) {
+            dummySequenceNumbers.add(tuple(i));
+        }
+
+        data.set("input", dummySequenceNumbers);
+
+        pigServer.registerQuery("A = load 'input' using mock.Storage();");
+        pigServer.registerQuery("B = foreach A generate $0 as col1;");
+        pigServer.registerQuery("C = foreach (group B all) generate COUNT($1);");
+        Iterator<Tuple> it = pigServer.openIterator("C");
+        assertTrue(it.hasNext());
+        Tuple t = it.next();
+        assertEquals(1000L, t.get(0));
+        assertFalse(it.hasNext());
+    }
+}
