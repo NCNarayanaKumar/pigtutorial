f6519853eeb2569c03269a1576f0571d412eb597
snayakm <snayakm@gmail.com>
2014-07-07 23:52:02 +0530

    Added Filter equivalent statement for the split

--- ch01/src/test/java/com/pigtutorial/ch01/TestSplit.java
+++ ch01/src/test/java/com/pigtutorial/ch01/TestSplit.java
@@ -46,4 +46,40 @@ public class TestSplit extends TestCase {
 			e.printStackTrace();
 		}
 	}
+	
+	@Test
+	public void testFilterEquivalentForSplitOperation() {
+		String[] args = {};
+		final String EN_STUD_EXPECTED_OUTPUT_FILE_PATH = "src/resources/data/expected_output/english_students_split_output";
+		final String MA_STUD_EXPECTED_OUTPUT_FILE_PATH = "src/resources/data/expected_output/mathematics_students_split_output";
+		final String ST_STUD_EXPECTED_OUTPUT_FILE_PATH = "src/resources/data/expected_output/statistics_students_split_output";
+		final String OT_STUD_EXPECTED_OUTPUT_FILE_PATH = "src/resources/data/expected_output/other_students_split_output";
+		// Specify the expected output file path and create file object
+		File enFile = new File(EN_STUD_EXPECTED_OUTPUT_FILE_PATH);
+		File maFile = new File(MA_STUD_EXPECTED_OUTPUT_FILE_PATH);
+		File stFile = new File(ST_STUD_EXPECTED_OUTPUT_FILE_PATH);
+		File otFile = new File(OT_STUD_EXPECTED_OUTPUT_FILE_PATH);
+
+		PigTest test = null;
+		try {
+			test = new PigTest("src/resources/pig/filter_equivalent_of_split.pig", args);
+		} catch (IOException e) {
+			System.err.println("Caught exception while reading pig script");
+			e.printStackTrace();
+		}
+
+		try {
+			// Test 4 split relations one by one
+			test.assertOutput("english_students", enFile);
+			test.assertOutput("mathematics_students", maFile);
+			test.assertOutput("statistics_students", stFile);
+			test.assertOutput("other_students", otFile);
+		} catch (IOException e) {
+			System.err.println("Caught exception while reading file");
+			e.printStackTrace();
+		} catch (ParseException e) {
+			System.err.println("Caught exception while parsing script");
+			e.printStackTrace();
+		}
+	}
 }
